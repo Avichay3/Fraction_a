c9d7fc86d53b22f3a86ae20c84748d770109adbb
Avichay3 <aviaimzina312@gmail.com>
2023-05-02 21:16:52 +0300

    add Test.cpp with some tests, need to implement

---
+++ Test.cpp
@@ -0,0 +1,54 @@
+#include "doctest.h"
+#include <iostream>
+#include "sources/Fraction.hpp"
+#include <algorithm>
+using namespace std;
+using namespace ariel;
+
+TEST_CASE ("Test the + (add) operator"){
+
+}
+
+TEST_CASE ("Test the - (substruct) operator"){
+    
+}
+
+TEST_CASE ("Test the * (multiply) operator"){
+    
+}
+
+TEST_CASE ("Test the / (devision) operator"){
+    
+}
+
+TEST_CASE ("Test the + (add) operator"){
+    
+}
+
+TEST_CASE ("Test devision by zero"){
+    
+}
+
+TEST_CASE ("Test comparison operators (<, >, ==, >=, <=)"){
+    
+}
+
+TEST_CASE ("Test if the constructor throw exeption when the denominator is zero"){
+    
+}
+
+TEST_CASE("Test increment and decrement operators with fractions"){
+   
+}
+
+TEST_CASE("Test I/O (input/output) operators with fractions"){
+   
+}
+
+TEST_CASE("Mix and check"){ //mix operators on some fractions and check the result
+    
+}
+
+
+
+
