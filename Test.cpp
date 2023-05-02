ed43985f3932265f29db2e89821ff07150b1cb53
Avichay3 <aviaimzina312@gmail.com>
2023-05-02 21:31:44 +0300

    fill some tests. close to finish

--- Test.cpp
+++ Test.cpp
@@ -6,28 +6,45 @@ using namespace std;
 using namespace ariel;
 
 TEST_CASE ("Test the + (add) operator"){
-
+    Fraction a (2,3);
+    Fraction b (1,3);
+    Fraction c (1,2);
+    Fraction d (1,4);
+    CHECK_NOTHROW(a+b == Fraction(1,1));
+    CHECK_NOTHROW(c+d == Fraction(3,4));
 }
 
 TEST_CASE ("Test the - (substruct) operator"){
-    
+    Fraction a(1,3);
+    Fraction b(2,3);
+    Fraction c(-1,3);
+    CHECK_NOTHROW(a-b == c);
+    CHECK_NOTHROW(c+b == a);
 }
 
 TEST_CASE ("Test the * (multiply) operator"){
-    
+    Fraction a(1, 3);
+    Fraction b(2, 6);
+    CHECK(f1*f2 == Fraction(1, 9));
 }
+
 
 TEST_CASE ("Test the / (devision) operator"){
-    
+    Fraction a(1, 2);
+    Fraction b(2, 4);
+    Fraction c(0, 1);
+    CHECK(((a/b == b/a) && (a/b == Fraction(1, 1))));
+    CHECK_THROWS(f2/f1);
 }
 
-TEST_CASE ("Test the + (add) operator"){
-    
-}
 
 TEST_CASE ("Test devision by zero"){
-    
+     CHECK_THROWS(Fraction(1, 0));
+     CHECK_NOTHROW(Fraction(1, -1));
+     CHECK_THROWS(Fraction(-2, 0));
+     CHECK_NOTHROW(Fraction(-10, -10));
 }
+
 
 TEST_CASE ("Test comparison operators (<, >, ==, >=, <=)"){
     
